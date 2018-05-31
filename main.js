9569ce8ef6917186f6ce4625f9b06a1e1488bf7a
William Kimball <wkkimball043@gmail.com>
2018-05-31 15:18:12 -0500

    added Lee

---
+++ main.js
@@ -0,0 +1,23 @@
+const kellyEnterprises = Object.create({}, {
+    business: {
+        value: "Kelly Enterprises"
+    },
+    employmentStart: {
+        value:"08-01-2017"
+    }, 
+    employmentEnd: {
+        value: "5-1-2018"
+    }
+})
+
+cosnt leeUniversity = Object.create({}, {
+    business: {
+        value: "Lee University"
+    },
+    employmentStart: {
+        value:"01-01-2017"
+    }, 
+    employmentEnd: {
+        value: "5-1-2018"
+    }
+})
