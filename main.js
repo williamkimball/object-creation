47e4eebfef4ebedbe73dc0bafcb498a90f5b9b1d
William Kimball <wkkimball043@gmail.com>
2018-06-01 10:15:52 -0500

    completed the advanced practice

--- main.js
+++ main.js
@@ -1,37 +1,114 @@
 const kellyEnterprises = Object.create({}, {
-    business: {
-        value: "Kelly Enterprises"
+    Business: {
+        value: "Kelly Enterprises",
+        enumerable: true
     },
-    employmentStart: {
-        value:"08-01-2017"
-    }, 
-    employmentEnd: {
-        value: "5-1-2018"
+    Employment_Start: {
+        value: "08-01-2017",
+        enumerable: true
+    },
+    Employment_End: {
+        value: "5-1-2018",
+        enumerable: true
+    },
+    Likes: {
+        valie: ["Never doing the same thing twice", "decent pay"],
+        enumerable: true
+    },
+    Dislikes: {
+        value: ["very unpredictable hours", "mega micromanagement"],
+        enumerable: true
     }
 })
 
 const leeUniversity = Object.create({}, {
-    business: {
-        value: "Lee University"
+    Business: {
+        value: "Lee University",
+        enumerable: true
     },
-    employmentStart: {
-        value:"01-01-2017"
-    }, 
-    employmentEnd: {
-        value: "5-1-2018"
+    Employment_Start: {
+        value: "01-01-2017",
+        enumerable: true
+    },
+    Employment_End: {
+        value: "5-1-2018",
+        enumerable: true
+    },
+    Likes: {
+        value: ["Getting to work for a great school", "working with great ELLs"],
+        enumerable: true
+    },
+    Lislikes: {
+        value: ["Low pay"],
+        enumerable: true
     }
 })
 
 const christianPublishersOutlet = Object.create({}, {
-    business: {
-        value: "Christian Publishers Outlet"
+    Business: {
+        value: "Christian Publishers Outlet",
+        enumerable: true
     },
-    employmentStart: {
-        value:"07-01-2014"
-    }, 
-    employmentEnd: {
-        value: "1-1-2016"
+    Employment_Start: {
+        value: "07-01-2014",
+        enumerable: true
     },
-    likes: ["Good friends", "Exposure to lots of great Christian books", "Most customers", "Was always busy and needed"],
-    dislikes: ["King James Onlyists", "Being responsible for the store without any authority", "Being stuck in the back with online orders", ]
+    Employment_End: {
+        value: "1-1-2016",
+        enumerable: true
+    },
+    Likes: {
+        value: ["Good friends", "Exposure to lots of great Christian books", "Most customers", "Was always busy and needed"],
+        enumerable: true
+    },
+    Dislikes: {
+        value: ["King James Onlyists", "Being responsible for the store without any authority", "Being stuck in the back with online orders"],
+        enumerable: true
+    }
 })
+
+let mkJobOb = (job, start, end, like, dislike) => {
+    let obj = Object.create({}, {
+        Business: {
+            value: job,
+            enumerable: true
+        },
+        Employment_Start: {
+            value: start,
+            enumerable: true
+        },
+        Employment_End: {
+            value: end,
+            enumerable: true
+        },
+        Likes: {
+            value: like,
+            enumerable: true
+        },
+        Dislikes: {
+            value: dislike,
+            enumerable: true
+        }
+
+    })
+    return obj;
+}
+let teachingAssistant = mkJobOb('TA', "01-01-2017", "05-30-2017", ["Freshman students", "Jones' teaching style"], ["Freshman students", "Subjectively grading papers", "Having to fail students"])
+
+let jobs = [teachingAssistant, kellyEnterprises, leeUniversity, christianPublishersOutlet]
+
+
+for (i = 0; i < jobs.length; i++) {
+    let jobArticle = document.createElement("article");
+    jobArticle.id = jobs[i].business;
+
+    for (property in jobs[i]) {
+        let liRef = document.createElement("p")
+        liRef.textContent = `${property}: ${jobs[i][property]}`
+        jobArticle.appendChild(liRef);
+    }
+
+    document.querySelector("#jobs").appendChild(jobArticle);
+}
+
+
